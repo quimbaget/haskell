1ff840091fbc16699279f5c40350b65f0704eb8a
Quim Baget Salarich <quim.baget@paack.co>
2021-11-21 19:03:30 +0100

    define main architecture

--- practica/propgraphs.hs
+++ practica/propgraphs.hs
@@ -1,6 +1,10 @@
+-- TYPE SYNONYMS
+
 type Label = String
 
 type Id = String
+
+-- CUSTOM DATA TYPES
 
 data Val = String | Int | Double | Bool | Date
 
@@ -15,3 +19,25 @@ data Edge
   = V Id
   | Origin Node
   | EdgeProps [Prop]
+
+data PG
+  = Nodes [Node]
+  | Edges [Edge]
+
+-- POPERTY GRAPH (PG) INSTANCES
+
+instance Show PG
+
+-- I/O FUNCTIONS
+
+-- PROPERTY GRAPHS METHODS
+
+populate :: String -> String -> String -> String -> PG
+addEdge :: PG -> Edge -> Prop -> PG
+defVprop :: PG -> Node -> Prop -> PG
+defEProp :: PG -> Edge -> Prop -> PG
+defVlabel :: PG -> Node -> Prop -> PG
+defElabel :: PG -> Edge -> Prop -> PG
+showGraph :: PG
+
+-- PROPERTY GRAPHS QUERIES
