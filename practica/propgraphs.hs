77cccc30f04fc5ee7f169fae93ef0a425624e965
Quim Baget Salarich <quim.baget@paack.co>
2021-11-22 20:41:02 +0100

    mi poya morena en vinagre haskell de los cojones

--- practica/propgraphs.hs
+++ practica/propgraphs.hs
@@ -6,38 +6,98 @@ type Id = String
 
 -- CUSTOM DATA TYPES
 
-data Val = String | Int | Double | Bool | Date
+data Prop = Prop Label String deriving (Show)
 
-data Prop = Name Label | Value (Maybe Val)
+data Node = Node Id Label [Prop] deriving (Show)
 
-data Node
-  = N Id
-  | Type Label
-  | Props [Prop]
+data Edge = Edge Id Id Id Label [Prop] deriving (Show) -- Edge Id, origin node ID, destination node ID, list of props
 
-data Edge
-  = V Id
-  | Origin Node
-  | EdgeProps [Prop]
-
-data PG
-  = Nodes [Node]
-  | Edges [Edge]
+data PG = PG [Node] [Edge] deriving (Show)
 
 -- POPERTY GRAPH (PG) INSTANCES
 
-instance Show PG
+{- instance Show Prop where
+  show (Prop l s) = "(" ++ show l ++ "," ++ show s ++ ")"
+
+instance Show Node where
+  show (Node id l props) = show id ++ "[" ++ show l ++ "]" ++ concatMap show props
+
+instance Show Edge where
+  show (Edge id n1 n2 label props) = "(" ++ show n1 ++ ") - " ++ show id ++ "[" ++ label ++ "] -> (" ++ show n2 ++ ")" ++ concatMap show props -}
+
+{- instance Show PG where
+  show (PG nodes edges) = (map show nodes) ++ (map show edges) -}
 
 -- I/O FUNCTIONS
 
+generateEdge :: [String] -> Edge
+generateEdge (e : (originNode : nodes)) = Edge e originNode (last nodes) "" []
+
+generateNode :: [String] -> Node
+generateNode (id : label) = Node id (last label) []
+
+generateProp :: String -> String -> Prop
+generateProp = Prop
+
+parseLine :: String -> [String]
+parseLine = words
+
+parseFile :: String -> [String]
+parseFile = lines
+
+parseRhoFile :: String -> [Edge]
+parseRhoFile file = map (generateEdge . parseLine) (parseFile file)
+
+filterNodes :: [Edge] -> [String] -> [String]
+filterNodes edges = filter (\x -> not $ edgeIsInside (head (parseLine x)) edges)
+
+parseLamdaFile :: String -> [Edge] -> [Node]
+parseLamdaFile file edges = map (generateNode . parseLine) (filterNodes edges (parseFile file))
+
 -- PROPERTY GRAPHS METHODS
 
+edgeIsEqual :: String -> Edge -> Bool
+edgeIsEqual edge (Edge id _ _ _ _) = edge == id
+
+edgeIsInside :: String -> [Edge] -> Bool
+edgeIsInside _ [] = False
+edgeIsInside edge (e : es) = edgeIsInside edge es || edgeIsEqual edge e
+
+hasEdge :: PG -> String -> Bool
+hasEdge (PG _ edges) edge = edgeIsInside edge edges
+
 populate :: String -> String -> String -> String -> PG
-addEdge :: PG -> Edge -> Prop -> PG
+populate rho lamda sigma props =
+  let edges = parseRhoFile rho
+      nodes = parseLamdaFile lamda edges
+   in PG nodes edges
+
+--PG nodes edges
+
+{- addEdge :: PG -> Edge -> Prop -> PG
 defVprop :: PG -> Node -> Prop -> PG
 defEProp :: PG -> Edge -> Prop -> PG
 defVlabel :: PG -> Node -> Prop -> PG
 defElabel :: PG -> Edge -> Prop -> PG
-showGraph :: PG
+showGraph :: PG -}
 
--- PROPERTY GRAPHS QUERIES
+main :: IO ()
+main = do
+  putStrLn "Input rhoFilePath"
+  path <- getLine
+  rhoFile <- readFile path
+
+  putStrLn "Input lamdaFile"
+  path <- getLine
+  lamdaFile <- readFile path
+
+  {-   putStrLn "Input sigmaFile"
+    path <- getLine
+    sigmaFile <- readFile path
+
+    putStrLn "Input propFile"
+    path <- getLine
+    propFile <- readFile path -}
+
+  let pg = populate rhoFile lamdaFile "sigmaFile" "propFile"
+  print pg
